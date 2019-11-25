b7ec4e772bef4525a48233c0f10e42a2ebe9b0d0
Xavi Aracil <xaracil@uoc.edu>
2019-11-25 13:07:57 +0100

    Added new class for creating LTI's dependants beans (ClaimsAccessor, TokenBuilders, etc)

---
+++ src/main/java/edu/uoc/elearn/lti/provider/config/LTIConfig.java
@@ -0,0 +1,32 @@
+package edu.uoc.elearn.lti.provider.config;
+
+import edu.uoc.elc.spring.security.lti.tool.BasicToolDefinition;
+import edu.uoc.lti.claims.ClaimAccessor;
+import edu.uoc.lti.clientcredentials.ClientCredentialsTokenBuilder;
+import edu.uoc.lti.deeplink.DeepLinkingTokenBuilder;
+import edu.uoc.lti.jwt.claims.JWSClaimAccessor;
+import edu.uoc.lti.jwt.client.JWSClientCredentialsTokenBuilder;
+import edu.uoc.lti.jwt.deeplink.JWSTokenBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+@Configuration
+public class LTIConfig {
+	@Bean
+	public ClaimAccessor claimAccessor(BasicToolDefinition basicToolDefinition) {
+		return new JWSClaimAccessor(basicToolDefinition.getKeySetUrl());
+	}
+
+	@Bean
+	public DeepLinkingTokenBuilder deepLinkingTokenBuilder(BasicToolDefinition basicToolDefinition) {
+		return new JWSTokenBuilder(basicToolDefinition.getPublicKey(), basicToolDefinition.getPrivateKey());
+	}
+
+	@Bean
+	public ClientCredentialsTokenBuilder clientCredentialsTokenBuilder(BasicToolDefinition basicToolDefinition) {
+		return new JWSClientCredentialsTokenBuilder(basicToolDefinition.getPublicKey(), basicToolDefinition.getPrivateKey());
+	}
+}
