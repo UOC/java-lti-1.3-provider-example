cfc87cf91aad59ae0dba1efbb289c1b905bc2e07
Xavi Aracil <xaracil@uoc.edu>
2019-11-26 15:07:40 +0100

    Added AccessTokenRequestBuilder

--- src/main/java/edu/uoc/elearn/lti/provider/config/LTIConfig.java
+++ src/main/java/edu/uoc/elearn/lti/provider/config/LTIConfig.java
@@ -1,6 +1,8 @@
 package edu.uoc.elearn.lti.provider.config;
 
 import edu.uoc.elc.spring.security.lti.tool.BasicToolDefinition;
+import edu.uoc.lti.accesstoken.AccessTokenRequestBuilder;
+import edu.uoc.lti.accesstoken.UrlEncodedFormAccessTokenRequestBuilderImpl;
 import edu.uoc.lti.claims.ClaimAccessor;
 import edu.uoc.lti.clientcredentials.ClientCredentialsTokenBuilder;
 import edu.uoc.lti.deeplink.DeepLinkingTokenBuilder;
@@ -28,5 +30,10 @@ public class LTIConfig {
 	@Bean
 	public ClientCredentialsTokenBuilder clientCredentialsTokenBuilder(BasicToolDefinition basicToolDefinition) {
 		return new JWSClientCredentialsTokenBuilder(basicToolDefinition.getPublicKey(), basicToolDefinition.getPrivateKey());
+	}
+
+	@Bean
+	public AccessTokenRequestBuilder accessTokenRequestBuilder() {
+		return new UrlEncodedFormAccessTokenRequestBuilderImpl();
 	}
 }
