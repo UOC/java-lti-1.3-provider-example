444cfdc7392f3bf81a18b7c61a369b01e7aa2caa
Xavi Aracil <xaracil@uoc.edu>
2019-11-28 15:13:28 +0100

    Removed unused import

--- src/main/java/edu/uoc/elearn/lti/provider/config/ApplicationSecurity.java
+++ src/main/java/edu/uoc/elearn/lti/provider/config/ApplicationSecurity.java
@@ -5,6 +5,7 @@ import edu.uoc.elearn.lti.provider.security.UOCLTIApplicationSecurity;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
