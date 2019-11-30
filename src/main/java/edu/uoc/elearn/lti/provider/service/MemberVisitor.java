0572cc1975b43c35b670508b29060235592632f1
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 16:15:17 +0100

    Interface definition for getting members

---
+++ src/main/java/edu/uoc/elearn/lti/provider/service/MemberVisitor.java
@@ -0,0 +1,12 @@
+package edu.uoc.elearn.lti.provider.service;
+
+import edu.uoc.elc.lti.platform.Member;
+
+import java.util.List;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+public interface MemberVisitor {
+	List<Member> getAll();
+}
