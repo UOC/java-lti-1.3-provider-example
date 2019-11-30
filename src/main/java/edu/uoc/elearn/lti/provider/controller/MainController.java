06e2dfca5799a4e9e2049a98ccea110555f2f3db
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 16:27:13 +0100

    Remove unused params and exceptions

--- src/main/java/edu/uoc/elearn/lti/provider/controller/MainController.java
+++ src/main/java/edu/uoc/elearn/lti/provider/controller/MainController.java
@@ -1,12 +1,8 @@
 package edu.uoc.elearn.lti.provider.controller;
 
 import edu.uoc.elc.lti.platform.Member;
-import edu.uoc.elc.lti.platform.ags.ToolLineItemServiceClient;
 import edu.uoc.elc.spring.lti.security.Context;
 import edu.uoc.elc.spring.lti.security.User;
-import edu.uoc.elc.spring.lti.tool.AgsServiceProvider;
-import edu.uoc.elc.spring.lti.tool.NamesRoleServiceProvider;
-import edu.uoc.elc.spring.lti.tool.ToolProvider;
 import edu.uoc.elearn.lti.provider.beans.IndexBean;
 import edu.uoc.elearn.lti.provider.service.LineItemVisitor;
 import edu.uoc.elearn.lti.provider.service.MemberVisitor;
@@ -19,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @Controller
@@ -32,12 +27,12 @@ public class MainController {
 	private final LineItemVisitor lineItemVisitor;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView init(User user, Context context, ToolProvider toolProvider) throws URISyntaxException {
-		final IndexBean response = generateResponseObject(user, context, toolProvider);
+	public ModelAndView init(User user, Context context) {
+		final IndexBean response = generateResponseObject(user, context);
 		return new ModelAndView("index.html", "object", response);
 	}
 
-	private IndexBean generateResponseObject(User user, Context context, ToolProvider toolProvider) throws URISyntaxException {
+	private IndexBean generateResponseObject(User user, Context context) {
 		final List<String> roles = user.getRoles();
 		final Boolean hasNamesRoleService = memberVisitor.canGet();
 		final List<Member> members = memberVisitor.getAll();
