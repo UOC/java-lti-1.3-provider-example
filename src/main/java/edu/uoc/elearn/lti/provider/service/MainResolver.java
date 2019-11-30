c3b358cef4fd40c3e43dea5e68692fe20bff668c
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 23:23:21 +0100

    Added interface for getting a recap of the tool

---
+++ src/main/java/edu/uoc/elearn/lti/provider/service/MainResolver.java
@@ -0,0 +1,12 @@
+package edu.uoc.elearn.lti.provider.service;
+
+import edu.uoc.elc.spring.lti.security.Context;
+import edu.uoc.elc.spring.lti.security.User;
+import edu.uoc.elc.spring.lti.tool.ToolProvider;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+public interface MainResolver {
+	Object getRecap(User user, Context context, ToolProvider toolProvider);
+}
