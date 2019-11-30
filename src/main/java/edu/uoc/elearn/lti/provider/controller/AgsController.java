d30a37c8357881bcf760b6d9917e5403bdaf3e9d
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 16:45:54 +0100

    AgsController use LineItemVisitor

--- src/main/java/edu/uoc/elearn/lti/provider/controller/AgsController.java
+++ src/main/java/edu/uoc/elearn/lti/provider/controller/AgsController.java
@@ -5,6 +5,7 @@ import edu.uoc.elc.spring.lti.tool.AgsServiceProvider;
 import edu.uoc.elc.spring.lti.tool.ToolProvider;
 import edu.uoc.elearn.lti.provider.beans.AgsBean;
 import edu.uoc.elearn.lti.provider.domain.LineItemFactory;
+import edu.uoc.elearn.lti.provider.service.LineItemVisitor;
 import edu.uoc.lti.ags.LineItem;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,11 +24,11 @@ import java.util.List;
 @Slf4j
 @PreAuthorize("isAuthenticated()")
 public class AgsController {
+	private final static String TAG = "UOC-TEST";
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView list(ToolProvider toolProvider) {
-		final ToolLineItemServiceClient lineItemServiceClient = getLineItemServiceClient(toolProvider);
-		final List<LineItem> lineItems = lineItemServiceClient.getLineItems(null, null, "UOC-TEST", null);
+		final List<LineItem> lineItems = getExistingLineItemsForTool(toolProvider);
 		AgsBean agsBean = new AgsBean(true, lineItems);
 		return new ModelAndView("ags/index", "object", agsBean);
 	}
@@ -40,9 +41,14 @@ public class AgsController {
 		return "redirect:/ags";
 	}
 
+	private List<LineItem> getExistingLineItemsForTool(ToolProvider toolProvider) {
+		LineItemVisitor lineItemVisitor = new LineItemVisitor(toolProvider);
+		return lineItemVisitor.getAll(null, null, TAG, null);
+	}
+
 	private LineItem createObject(String label, Double maxScore) {
 		LineItemFactory lineItemFactory = new LineItemFactory();
-		return lineItemFactory.newLineItem(label, maxScore, "UOC-TEST");
+		return lineItemFactory.newLineItem(label, maxScore, TAG);
 	}
 
 	private ToolLineItemServiceClient getLineItemServiceClient(ToolProvider toolProvider) {
