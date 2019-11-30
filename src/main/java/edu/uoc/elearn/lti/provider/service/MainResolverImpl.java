4f44c029ce964077fe8793a1b5c88984f2a3edf0
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 23:26:02 +0100

    Added service

--- src/main/java/edu/uoc/elearn/lti/provider/service/MainResolverImpl.java
+++ src/main/java/edu/uoc/elearn/lti/provider/service/MainResolverImpl.java
@@ -10,29 +10,31 @@ import edu.uoc.elearn.lti.provider.beans.NamesRoleBean;
 import edu.uoc.elearn.lti.provider.service.ags.LineItemVisitor;
 import edu.uoc.elearn.lti.provider.service.ags.MemberVisitor;
 import edu.uoc.lti.ags.LineItem;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * @author Xavi Aracil <xaracil@uoc.edu>
  */
+@Service
 public class MainResolverImpl implements MainResolver {
 	@Override
 	public IndexBean getRecap(User user, Context context, ToolProvider toolProvider) {
 		final List<String> roles = user.getRoles();
-		final NamesRoleBean namesRolesServiceInfo = createBeanWithNamesRoleServiceInfo(toolProvider);
-		final AgsBean assignmentAndGradesServiceInfo = createBeanWithAssignmentAndGradesServiceInfo(toolProvider);
+		final NamesRoleBean namesRolesServiceInfo = getNamesRoleServiceRecap(toolProvider);
+		final AgsBean assignmentAndGradesServiceInfo = getAssignmentGradeServiceRecap(toolProvider);
 		return new IndexBean(user, context, roles, namesRolesServiceInfo, assignmentAndGradesServiceInfo);
 	}
 
-	private NamesRoleBean createBeanWithNamesRoleServiceInfo(ToolProvider toolProvider) {
+	private NamesRoleBean getNamesRoleServiceRecap(ToolProvider toolProvider) {
 		final MemberVisitor memberVisitor = new MemberVisitor(toolProvider);
 		final Boolean available = memberVisitor.canGet();
 		final List<Member> members = memberVisitor.getAll();
 		return new NamesRoleBean(available, members);
 	}
 
-	private AgsBean createBeanWithAssignmentAndGradesServiceInfo(ToolProvider toolProvider) {
+	private AgsBean getAssignmentGradeServiceRecap(ToolProvider toolProvider) {
 		final LineItemVisitor lineItemVisitor = new LineItemVisitor(toolProvider);
 		final Boolean available = lineItemVisitor.canGet();
 		final List<LineItem> lineItems = lineItemVisitor.getAll();
