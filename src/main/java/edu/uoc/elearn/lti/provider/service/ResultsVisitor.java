d9eac6a31427d43dd9de9c6c58988796388ceda8
Xavi Aracil <xaracil@uoc.edu>
2019-11-30 17:28:03 +0100

    Added visitor for AGS' result service

---
+++ src/main/java/edu/uoc/elearn/lti/provider/service/ResultsVisitor.java
@@ -0,0 +1,36 @@
+package edu.uoc.elearn.lti.provider.service;
+
+import edu.uoc.elc.lti.platform.ags.GenericResultServiceClient;
+import edu.uoc.elc.spring.lti.tool.AgsServiceProvider;
+import edu.uoc.elc.spring.lti.tool.ToolProvider;
+import edu.uoc.lti.ags.Result;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+@RequiredArgsConstructor
+public class ResultsVisitor {
+	private final ToolProvider toolProvider;
+
+	public boolean canGet() {
+		final AgsServiceProvider agsServiceProvider = toolProvider.getAgsServiceProvider();
+		return agsServiceProvider.hasAgsService();
+	}
+
+	public List<Result> getAll(String id) {
+		return getAll(id, null, null, null);
+	}
+
+	public List<Result> getAll(String id, Integer limit, Integer page, String userId) {
+		final GenericResultServiceClient serviceClient = getServiceClient();
+		return serviceClient.getLineItemResults(id, limit, page, userId);
+	}
+
+	private GenericResultServiceClient getServiceClient() {
+		final AgsServiceProvider agsServiceProvider = toolProvider.getAgsServiceProvider();
+		return agsServiceProvider.getResultsServiceClient();
+	}
+}
