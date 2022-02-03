98f4dfe2a208a1dd668bf9857918007eac97dbca
Xavi Aracil <xaracil@uoc.edu>
2022-02-03 18:16:07 +0100

    Get services trough spring-boot-advantage-jwks library

--- src/main/java/edu/uoc/elearn/lti/provider/config/LTIConfig.java
+++ src/main/java/edu/uoc/elearn/lti/provider/config/LTIConfig.java
@@ -1,38 +1,18 @@
 package edu.uoc.elearn.lti.provider.config;
 
 import edu.uoc.elc.spring.lti.ags.RestTemplateFactory;
-import edu.uoc.elc.spring.lti.tool.BasicToolDefinition;
 import edu.uoc.lti.accesstoken.AccessTokenRequestBuilder;
 import edu.uoc.lti.accesstoken.UrlEncodedFormAccessTokenRequestBuilderImpl;
-import edu.uoc.lti.claims.ClaimAccessor;
-import edu.uoc.lti.clientcredentials.ClientCredentialsTokenBuilder;
-import edu.uoc.lti.deeplink.DeepLinkingTokenBuilder;
-import edu.uoc.lti.jwt.claims.JWSClaimAccessor;
-import edu.uoc.lti.jwt.client.JWSClientCredentialsTokenBuilder;
-import edu.uoc.lti.jwt.deeplink.JWSTokenBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xaracil@uoc.edu
  */
 @Configuration
+@ComponentScan(value = {"edu.uoc.elc.spring.lti.tool.builders"})
 public class LTIConfig {
-	@Bean
-	public ClaimAccessor claimAccessor(BasicToolDefinition basicToolDefinition) {
-		return new JWSClaimAccessor(basicToolDefinition.getKeySetUrl());
-	}
-
-	@Bean
-	public DeepLinkingTokenBuilder deepLinkingTokenBuilder(BasicToolDefinition basicToolDefinition) {
-		return new JWSTokenBuilder(basicToolDefinition.getPublicKey(), basicToolDefinition.getPrivateKey());
-	}
-
-	@Bean
-	public ClientCredentialsTokenBuilder clientCredentialsTokenBuilder(BasicToolDefinition basicToolDefinition) {
-		return new JWSClientCredentialsTokenBuilder(basicToolDefinition.getPublicKey(), basicToolDefinition.getPrivateKey());
-	}
-
 	@Bean
 	public AccessTokenRequestBuilder accessTokenRequestBuilder() {
 		return new UrlEncodedFormAccessTokenRequestBuilderImpl();
